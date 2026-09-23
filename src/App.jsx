74c6856fb5b97b624ffac4ebfb353f36d1e6956a
suhanijoshi20 <joshisuhani56@gmail.com>
2026-09-23 16:04:57 +0530

    Fix Expertise component casing issue for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
-import Expertise from './components/Expertise';
+import Expertise from './components/Expertise'; // Case sensitive: Check exact name in components folder
 import Gallery from './components/Gallery';
 import Contact from './components/Contact';
 
